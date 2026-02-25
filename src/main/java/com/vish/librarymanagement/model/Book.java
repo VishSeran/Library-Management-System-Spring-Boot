1fb828f6f34e54da06121d4cb143857e825797d5
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 11:11:11 +0530

    Initialize the project and created the book model

---
+++ src/main/java/com/vish/librarymanagement/model/Book.java
@@ -0,0 +1,12 @@
+package com.vish.librarymanagement.model;
+
+public class Book {
+    private long id;
+
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String genre;
+    private int availableCopies;
+
+}
