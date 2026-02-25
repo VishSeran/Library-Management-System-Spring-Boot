786cc4a52a3dab9335ccbf669387c662dddb181a
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 11:57:52 +0530

    Borrowingrecord model methods updated

--- src/main/java/com/vish/librarymanagement/model/BorrowingRecord.java
+++ src/main/java/com/vish/librarymanagement/model/BorrowingRecord.java
@@ -29,6 +29,66 @@ public class BorrowingRecord {
 
     }
 
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "BorrowingRecord{" +
+                "id=" + id +
+                ", book=" + book +
+                ", member=" + member +
+                ", borrowDate=" + borrowDate +
+                ", returnDate=" + returnDate +
+                ", dueDate=" + dueDate +
+                '}';
+    }
 
 }
