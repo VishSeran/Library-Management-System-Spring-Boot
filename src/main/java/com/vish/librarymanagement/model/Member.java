6faac40fe911155140e1ae9fcbeea68d20d9f32b
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 11:41:13 +0530

    Member model updated

--- src/main/java/com/vish/librarymanagement/model/Member.java
+++ src/main/java/com/vish/librarymanagement/model/Member.java
@@ -25,4 +25,66 @@ public class Member {
         this.membershipEndDate = membershipEndDate;
     }
 
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", membershipStartDate=" + startDate +
+                ", membershipEndDate=" + endDate +
+                '}';
+    }
+
 }
