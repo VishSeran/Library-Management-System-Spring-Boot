1be804167166f53bd4813e68159a68b0c6e5ccad
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 14:18:27 +0530

    library service Member CRUD updated

--- src/main/java/com/vish/librarymanagement/services/LibraryService.java
+++ src/main/java/com/vish/librarymanagement/services/LibraryService.java
@@ -2,10 +2,11 @@ package com.vish.librarymanagement.services;
 
 import com.vish.librarymanagement.model.Book;
 import com.vish.librarymanagement.model.BorrowingRecord;
+import com.vish.librarymanagement.model.Member;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.Member;
+
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -16,6 +17,7 @@ public class LibraryService {
     private ArrayList<Member> membersList = new ArrayList<>();
     private ArrayList<BorrowingRecord> borrowingRecordList = new ArrayList<>();
 
+    //===========Book CRUD===============================
 
     //get all books
     public ArrayList<Book> getAllBooks() {
@@ -49,5 +51,42 @@ public class LibraryService {
     public void deleteBookById(Long id){
         booksList.removeIf(book -> book.getId().equals(id));
     }
+
+    //=========Member CRUD======================
+
+    //Get all members
+    public ArrayList<Member> getAllMembers() {
+        return membersList;
+    }
+
+    //Get member by ID
+    public Optional<Member> getMemberById(Long id){
+        return membersList.stream().filter(
+                member -> member.getId().equals(id)
+        ).findFirst();
+    }
+
+    //Add a new member
+    public void addMember (Member member) {
+        membersList.add(member);
+    }
+
+    //update a member
+    public void updateMember(Member updatedMember) {
+        for(int i =0; i < membersList.size(); i++){
+            Member member = membersList.get(i);
+            if(member.getId().equals(updatedMember.getId())){
+                membersList.set(i, updatedMember);
+                break;
+            }
+        }
+    }
+
+    //Delete member by ID
+    public void deleteMember(Long id) {
+        membersList.removeIf(member -> member.getId().equals(id));
+    }
+
+
 
 }
