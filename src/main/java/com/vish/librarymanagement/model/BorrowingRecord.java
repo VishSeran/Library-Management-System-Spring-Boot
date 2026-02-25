1d8ca85bf7c28b6ab038d0716487d81bd22d6ab9
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 11:57:09 +0530

    Borrowingrecord model updated

--- src/main/java/com/vish/librarymanagement/model/BorrowingRecord.java
+++ src/main/java/com/vish/librarymanagement/model/BorrowingRecord.java
@@ -1,9 +1,34 @@
 package com.vish.librarymanagement.model;
 
 import javax.sound.midi.MetaMessage;
+import java.time.LocalDate;
 
 public class BorrowingRecord {
 
+    private Long id;
+
     private Book book;
     private Member member;
+
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+    private LocalDate dueDate;
+
+    //Default constructor
+    public BorrowingRecord() {
+
+    }
+
+    // Parameterized constructor
+    public BorrowingRecord (Book book, Member member, LocalDate borrowDate, LocalDate returnDate, LocalDate dueDate) {
+        this.book = book;
+        this.member = member;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+        this.dueDate = dueDate;
+
+    }
+
+
+
 }
