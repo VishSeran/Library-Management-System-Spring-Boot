8d9aab253adbd18c3a85dd7cb7326594ecb2145f
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 16:20:21 +0530

    library controller Get mapping get book by id

--- src/main/java/com/vish/librarymanagement/controller/LibraryController.java
+++ src/main/java/com/vish/librarymanagement/controller/LibraryController.java
@@ -1,11 +1,20 @@
 package com.vish.librarymanagement.controller;
 
+import com.vish.librarymanagement.model.Book;
 import com.vish.librarymanagement.services.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,4 +28,19 @@ public class LibraryController {
 
     //============Book Endpoints=============
 
+    @GetMapping("/books")
+    public ResponseEntity<ArrayList<Book>> getAllBooks () {
+        ArrayList<Book> books = libraryService.getAllBooks();
+        logger.info("The list of book returned: " + books);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    //Get book by id
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = libraryService.getBookById(id);
+        logger.info("Get book by id: " + book);
+        return book.map( value -> new ResponseEntity<>(value, HttpStatus.OK)).
+                orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
